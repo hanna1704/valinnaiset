dfb99fe4e2d3e9052cc5f28db9d5a1621ce119fc
Hanna Lampola <hanna.lampola.hl@gmail.com>
2019-05-16 11:13:04 +0300

    Tiedostojen lisäys tietokantaan

--- src/App.vue
+++ src/App.vue
@@ -142,7 +142,7 @@ export default {
          nimi: this.name, 
          luokka: this.luokka, 
          pvm: new Date(), 
-         valinnat: this.valitutpakoliset.pakolliset.join("\n")  + "\n" + this.valitutvalinnaiset.join("\n")
+         valinnat: this.valitutpakoliset.join("\n")  + "\n" + this.valitutvalinnaiset.join("\n")
       }
 
        var obj = new db.vastaukset(vastaus);
